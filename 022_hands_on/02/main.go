bc3d184438a842db1b568307cb936e66dbf7b698
Luis Henrique Witz <luiswitz@gmail.com>
2017-04-21 17:18:45 -0300

    Exercises

---
+++ 022_hands_on/02/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.HandleFunc("/dog/", bar)
+	http.HandleFunc("/me/", luis)
+	http.ListenAndServe(":8080", nil)
+}
+
+func foo(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "foo ran")
+}
+
+func bar(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "bar ran")
+}
+
+func luis(w http.ResponseWriter, req *http.Request) {
+	tpl, err := template.ParseFiles("tpl.gohtml")
+	if err != nil {
+		log.Fatalln("error parsing template", err)
+	}
+
+	err = tpl.ExecuteTemplate(w, "tpl.gohtml", "Luis")
+	if err != nil {
+		log.Fatalln("error executing template", err)
+	}
+}
